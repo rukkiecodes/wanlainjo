9be708d7ce35fdf80da1eb0e39894cd4eabace59
rukkiecodes <rukkiecodes@gmail.com>
2023-01-12 14:48:04 +0100

    Graphic Designer

--- src/components/ViewCouseDetails.vue
+++ src/components/ViewCouseDetails.vue
@@ -1,84 +1,123 @@
 <template>
-    <v-dialog v-model="viewCourse.dialog" fullscreen>
-        <v-card>
-            <v-toolbar dark color="indigo-darken-4">
-                <v-toolbar-title>{{ viewCourse.currentCourse.title }}</v-toolbar-title>
-                <v-spacer />
-                <v-btn dark class="bg-indigo-darken-3 text-capitalize" @click="viewCourse.dialog = false">
-                    Close
-                </v-btn>
-                <v-btn class="bg-white ml-4 text-capitalize" :to="`/register/${viewCourse.currentCourse.title}`" @click="viewCourse.dialog = false">
-                    Apply Now
-                </v-btn>
-            </v-toolbar>
+  <v-dialog v-model="viewCourse.dialog" fullscreen>
+    <v-card>
+      <v-toolbar dark color="indigo-darken-4">
+        <v-toolbar-title>{{ viewCourse.currentCourse.title }}</v-toolbar-title>
+        <v-spacer />
+        <v-btn
+          dark
+          class="bg-indigo-darken-3 text-capitalize"
+          @click="viewCourse.dialog = false"
+        >
+          Close
+        </v-btn>
+        <v-btn
+          class="bg-white ml-4 text-capitalize"
+          :to="`/register/${viewCourse.currentCourse.title}`"
+          @click="viewCourse.dialog = false"
+        >
+          Apply Now
+        </v-btn>
+      </v-toolbar>
 
-            <v-card-text>
-                <v-row justify="space-between">
-                    <v-col cols="12" sm="6" md="5">
-                        <v-card flat>
-                            <v-img :src="viewCourse.currentCourse.image" />
-                        </v-card>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="7">
-                        <v-card flat>
-                            <v-card-title class="text-h5 text-md-h3 font-weight-regular">{{
-                                viewCourse.currentCourse.title
-                            }}</v-card-title>
-                            <v-card-text class="text-body-2 mt-4">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus eveniet nemo,
-                                quam earum nostrum asperiores atque sapiente officia error eos assumenda quod laborum
-                                eligendi quos repellat, commodi rerum soluta facilis.
-                            </v-card-text>
-                            <v-card-text>
-                                <v-timeline v-for="(item, i) in 5" :key="i">
-                                    <v-timeline-item size="x-small">
-                                        <template v-slot:icon>
-                                            <v-avatar color="indigo" size="small">
-                                                {{ i+ 1 }}
-                                            </v-avatar>
-                                        </template>
-                                        <template v-slot:opposite>
-                                            <span class="d-none d-md-flex text-caption font-weight-bold">Month</span>
-                                        </template>
-                                        <v-card class="elevation-2">
-                                            <v-card-title class="text-h5">
-                                                Lorem ipsum
-                                            </v-card-title>
-                                            <v-card-text>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune
-                                                scaevola imperdiet nec ut, sed euismod convenire principes at. Est et
-                                                nobis iisque percipit, an vim zril disputando voluptatibus, vix an
-                                                salutandi sententiae.</v-card-text>
-                                        </v-card>
-                                    </v-timeline-item>
-                                </v-timeline>
-                            </v-card-text>
-                        </v-card>
-                    </v-col>
-                </v-row>
-            </v-card-text>
-        </v-card>
-    </v-dialog>
+      <v-container>
+        <v-card-text>
+          <v-row justify="space-between">
+            <v-col cols="12" sm="6" md="5">
+              <v-card flat>
+                <v-img :src="viewCourse.currentCourse.image" />
+              </v-card>
+            </v-col>
+            <v-col cols="12" sm="6" md="7">
+              <v-card flat>
+                <v-card-text
+                  style="line-height: 1em"
+                  class="text-h5 text-md-h4 font-weight-regular"
+                  >{{ viewCourse.currentCourse.subtext }}</v-card-text
+                >
+                <v-card-text class="text-body-2">{{
+                  viewCourse.currentCourse.body
+                }}</v-card-text>
+                <v-card-text>
+                  <v-timeline
+                    v-for="(outline, i) in viewCourse.currentCourse.outline"
+                    :key="i"
+                  >
+                    <v-timeline-item size="x-small">
+                      <template v-slot:icon>
+                        <v-avatar color="indigo" size="small">
+                          {{ i + 1 }}
+                        </v-avatar>
+                      </template>
+                      <template v-slot:opposite>
+                        <span
+                          class="d-none d-md-flex text-caption font-weight-bold"
+                          >Month</span
+                        >
+                      </template>
+                      <v-card class="elevation-2" width="500">
+                        <v-list>
+                          <v-list-item :title="outline.title">
+                            <template v-slot:append>
+                              <v-btn
+                                @click="outline.show = !outline.show"
+                                color="grey-lighten-1"
+                                :icon="
+                                  outline.show
+                                    ? 'mdi-chevron-up'
+                                    : 'mdi-chevron-down'
+                                "
+                                variant="text"
+                              ></v-btn>
+                            </template>
+                          </v-list-item>
+                        </v-list>
+                        <v-expand-transition>
+                          <v-card-text v-show="outline.show">
+                            <v-list density="compact">
+                              <v-list-item
+                                v-for="(item, i) in outline.breakdown"
+                                :key="i"
+                                density="compact"
+                                class="py-0 text-caption"
+                                :title="item"
+                              >
+                              </v-list-item>
+                            </v-list>
+                          </v-card-text>
+                        </v-expand-transition>
+                      </v-card>
+                    </v-timeline-item>
+                  </v-timeline>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-container>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 export default {
-    data: () => ({
-        dialog: false,
-        notifications: true,
-        sound: true,
-        widgets: true
-    }),
+  data: () => ({
+    dialog: false,
+    notifications: true,
+    sound: true,
+    widgets: true,
+  }),
 
-    computed: {
-        ...mapState(["viewCourse"])
-    }
-}
+  computed: {
+    ...mapState(["viewCourse"]),
+  },
+};
 </script>
 
 <style>
 .dialog-bottom-transition-enter-active,
 .dialog-bottom-transition-leave-active {
-    transition: transform .2s ease-in-out;
+  transition: transform 0.2s ease-in-out;
 }
 </style>
--- src/store/modules/courses.js
+++ src/store/modules/courses.js
@@ -8,7 +8,133 @@ const state = {
         {
             image: "https://res.cloudinary.com/rukkiecodes/image/upload/v1673280091/wanlainjo/courses/graphic-design_axrnad.jpg",
             title: 'Graphic Design',
-            body: 'CorelDraw, Adobe Photoshop, Adobe Illustrator, Adobe InDesign, Canva',
+            subtext: "Become a Professional Graphic Designer",
+            body: 'Understand your audience and business goals. Design delightful visuals and improve viewers’ engagement and conversion',
+            outline: [
+                {
+                    title: 'Introduction to Graphic Design',
+                    show: true,
+                    breakdown: [
+                        "Introduction & Welcome to the Course!",
+                        "What is Graphic Design?",
+                        "What is a Graphic Designer?",
+                        "Get Up and Running With Adobe Creative Cloud"
+                    ]
+                },
+                {
+                    title: 'BASICS OF ADOBE ILLUSTRATOR',
+                    show: false,
+                    breakdown: [
+                        "Introduction to the Project",
+                        "Understanding the Workspace",
+                        "Understanding the Tools",
+                        "Understanding the Panels",
+                        "Working With Text: Type Tools, Adobe Type kit, and Creating Outlines",
+                        "Artboards and Creating Additional Designs",
+                        "Exporting Your Files"
+                    ]
+                },
+                {
+                    title: 'LEARNING PHOTOSHOP (CREATING A POSTER)',
+                    show: false,
+                    breakdown: [
+                        "Poster Introduction and file setup (revised)",
+                        "Gathering Assets",
+                        "Refining Selections Update: The New",
+                        "Photoshop 2015.5 Selection and Masking Space",
+                        "Refining Selections",
+                        "IWorking With Text",
+                        "Refining the Layout",
+                        "Creating Additional Looks to Give Your Client Options",
+                        "Exporting Your Artwork",
+                    ]
+                },
+                {
+                    title: 'CREATING A LOGO',
+                    show: false,
+                    breakdown: [
+                        "Introduction to This Section",
+                        "Research and Gathering Ideas",
+                        "Creating a Mood board",
+                        "Bring in Your Text and Picking Fonts",
+                        "Basic Tool Overview",
+                        "Creating Your First Looks",
+                        "Refining Ideas",
+                        "Designing Your Presentation For Your Clients",
+                        "Exporting and Sending Comps to Clients",
+                        "Finalize Your Logo, Part 1: Refining Color and Creating a 'Details' Page",
+                        "Finalize Your Logo, Part 2: Artboard Setup for Quick Export and File Management",
+                        "Finalize Your Logo, Part 3: Batch Export Files and Professional Client Delivery",
+                    ]
+                },
+                {
+                    title: 'LEARNING PHOTOSHOP (CREATING A COMPOSITE IMAGE)',
+                    show: false,
+                    breakdown: [
+                        "Introduction to Photoshop Compositing",
+                        "Getting assets for your first composite image",
+                        "Compositing Continued & Color Levels",
+                        "Refining Selections",
+                        "Pushing Your Design Further",
+                        "One More Idea & Exporting Your Image"
+                    ]
+                },
+                {
+                    title: 'CREATING A MAGAZINE AD',
+                    show: false,
+                    breakdown: [
+                        "Project Introduction",
+                        "Setting Up the File",
+                        "Gathering Assets and How to Scrape Photos",
+                        "Getting Things in Place",
+                        "Design Continued.",
+                        "Further Refinement, Adding Photos, and Masking with Shapes",
+                        "Export the Final Ad for Press & Reviewing Design Decisions",
+                        "How to Package Editable Photoshop Files to Share With Someone Else"
+                    ]
+                },
+                {
+                    title: 'BASICS OF INDESIGN (CREATING AN EBOOK)',
+                    show: false,
+                    breakdown: [
+                        "Introduction and File Setup",
+                        "Placing Assets and Basic Layout",
+                        "Finalize the Design, Exporting, and Packaging Final Files"
+                    ]
+                },
+                {
+                    title: 'BASICS OF INDESIGN (CREATING A BROCHURE)',
+                    show: false,
+                    breakdown: [
+                        "Project Introduction",
+                        "Beginning With the End in Mind",
+                        "File Setup",
+                        "Outside Layout: Part 1",
+                        "Outside Layout: Part 2",
+                        "Inside Layout",
+                        "Exporting Final Files, Part 1: Generic Export",
+                        "Exporting Final Files, Part 2: Adding Printer Marks and Fold Guides in InDesign",
+                        "Exporting Final Files, Part 3: Add Printer Marks and Fold Guides in Illustrator"
+                    ]
+                },
+                {
+                    title: 'CREATING CUSTOM BUSINESS CARDS',
+                    show: false,
+                    breakdown: [
+                        "Project Introduction & File Setup",
+                        "Business Card Design",
+                        "Exporting Final Files and Custom Finishes"
+                    ]
+                },
+                {
+                    title: 'INTRODUCTION TO BEHANCE',
+                    show: false,
+                    breakdown: [
+                        "Features overview of behance",
+                        "Create a custom cover and full width portfolio image for behance"
+                    ]
+                },
+            ]
         },
         {
             image: "https://res.cloudinary.com/rukkiecodes/image/upload/v1673280089/wanlainjo/courses/front-end_c9bulq.jpg",
