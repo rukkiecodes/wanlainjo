8ccc9a2e0ccbf0a4c8b0198eb3bbf67e9ee88677
rukkiecodes <rukkiecodes@gmail.com>
2023-01-13 01:52:10 +0100

    done

--- src/components/JoinCommunity.vue
+++ src/components/JoinCommunity.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-btn to="/community" class="rounded-xl text-body-2 bg-indigo-darken-4" size="large" flat>
+    <v-btn to="/community" class="text-body-2 bg-indigo-darken-4" size="large" flat>
         Join Us Now!
     </v-btn>
 </template>
