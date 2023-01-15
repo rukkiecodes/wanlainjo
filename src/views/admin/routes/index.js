7dcaaceb6678f7b44a9c4cb35ef1070ccfd88fde
rukkiecodes <rukkiecodes@gmail.com>
2023-01-16 00:02:07 +0100

    fix route

--- src/views/admin/routes/index.js
+++ src/views/admin/routes/index.js
@@ -9,5 +9,20 @@ export default {
             name: 'admin.dashboard',
             component: () => import("@/views/admin/views/Overview.vue"),
         },
+        {
+            path: 'students',
+            name: 'admin.students',
+            component: () => import("@/views/admin/views/Students.vue"),
+        },
+        {
+            path: 'courses',
+            name: 'admin.courses',
+            component: () => import("@/views/admin/views/Courses.vue"),
+        },
+        {
+            path: 'instructors',
+            name: 'admin.instructors',
+            component: () => import("@/views/admin/views/Instructors.vue"),
+        },
     ]
 }
---
+++ src/views/admin/views/Instructors.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>instructors</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
